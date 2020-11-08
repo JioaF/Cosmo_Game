58a06117cef5361099e03e1f8678a4cd8fb70e82
JioaF <70670116+JioaF@users.noreply.github.com>
2020-11-08 21:19:55 +0700

    Commit

---
+++ Cosmonaut/Assets/script/Gunscript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+// script Refrence : https://www.youtube.com/watch?v=THnivyG0Mvo&t=69s
+// not yet finished 
+public class Gunscript : MonoBehaviour
+{
+    public float damage = 1f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Cosmonaut/Assets/script/Target.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+// script Refrence : https://www.youtube.com/watch?v=THnivyG0Mvo&t=69s
+// not yet finished
+public class Target : MonoBehaviour
+{
+    public float hp = 5f;
+     
+    public void TakeDamage(float amount)
+    {
+        hp -= amount;
+        if (hp <= 0)
+        {
+            Destroyed();
+        }
+    }
+
+    void Destroyed()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
